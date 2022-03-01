5337dfef19d55fcd70482801a920224d88186b96
RemyFrenchy <rme.guerin@gmail.com>
2022-03-01 11:41:24 +0100

    set marvel characters map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://remy-marvel-backend.herokuapp.com/characters"
+        );
+        console.log(response.data.results);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
+  return isLoading ? (
+    <span>En cours de chargement</span>
+  ) : (
+    <div>
+      {data.results.map((character, _id) => {
+        return <span key={_id}>{character.name}</span>;
+      })}
     </div>
   );
 }
