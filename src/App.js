9c9fc88d7f378d29a4c0ea4249511f82c964dc5b
RemyFrenchy <rme.guerin@gmail.com>
2022-03-01 15:28:47 +0100

    add pages & characters descritption

--- src/App.js
+++ src/App.js
@@ -5,27 +5,52 @@ import { useState, useEffect } from "react";
 function App() {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://remy-marvel-backend.herokuapp.com/characters"
+          `https://remy-marvel-backend.herokuapp.com/characters?page=${page}`
         );
-        console.log(response.data.results);
+
         setData(response.data);
+
         setIsLoading(false);
       } catch (error) {
         console.log(error.response);
       }
     };
     fetchData();
-  }, []);
+  }, [page]);
   return isLoading ? (
     <span>En cours de chargement</span>
   ) : (
     <div>
+      <button
+        style={{ display: page === 1 ? "none" : "inline" }}
+        onClick={() => {
+          setPage(page - 1);
+        }}
+      >
+        page précédente
+      </button>
+
+      <button
+        style={{
+          display: page === Math.ceil(data.count / 100) ? "none" : "inline",
+        }}
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        page suivante
+      </button>
       {data.results.map((character, _id) => {
-        return <span key={_id}>{character.name}</span>;
+        return (
+          <div key={_id}>
+            {character.name} : {character.description}
+          </div>
+        );
       })}
     </div>
   );
